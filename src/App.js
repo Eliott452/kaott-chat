203cadd4816af0beb4119c5786dfc90a8096fdaa
assi.karim <karimassi02@gmail.com>
2022-11-09 16:30:55 +0100

    signinout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import firebaseConfig from './private.js';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// const analytics = getAnalytics(app);
+
 
 function App() {
+const [user] = useAuthState(auth)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          bjr <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <header>
+    <h1> Kaott ChatBox </h1>
+    <Logout/>
+      
+    </header>
+
+    <section>
+    { user ?   <Chat/> : <Login/> }
+    </section>
+  </div>
+
   );
 }
 
-export default App;
+function Login() {
+  const googleSignIn = ()  => {
+    const provider = new GoogleAuthProvider()
+  signInWithPopup(auth, provider)
+ }
+
+return (
+  <>
+<button onClick={googleSignIn}> Sign in with Google </button>
+</>
+)
+}
+
+function Logout() {
+const googleSignOut = () => { signOut (auth)}
+return (
+  
+  auth.currentUser && <> <button onClick={googleSignOut}> Sign Out </button> </>
+  
+ 
+)
+}
+
+
+
+function Chat(){
+console.log(auth)
+return (<h1> Chatroom</h1>)
+
+}
+export default App
